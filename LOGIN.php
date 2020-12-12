e6ef11fcade64411898a7d7d5514be41c51e736a
kjvaldez09 <valdez.kennethjoseph@ue.edu.ph>
2020-12-12 18:17:21 +0800

    LOGIN PHP with data validation

--- LOGIN.php
+++ LOGIN.php
@@ -14,6 +14,70 @@ if(isset($_POST['logup'])){
 			'password' => $_POST['password']
 		)
 	);
+	
+	$results = $statement->fetch();
+	
+
+        if($results['username'] == $email and $results['password'] == $password) {
+            header("Location: ../PROJECTCOBRA/index1.html");
+        
+        }
+
+
+        if ($username == "" and $password == "") {
+            echo '<script>
+                alert ("Input your Username and Passsword");
+            </script>';
+            echo '<script>
+                window.history.go(-1);
+            </script>';
+            }
+
+        else if ($username == "") {
+            echo '<script>
+                alert ("Input your Username ");
+            </script>';
+            echo '<script>
+                 window.history.go(-1);
+            </script>';
+
+        }
+        else if ($password == "") {
+            echo '<script>
+                alert ("Input your Passsword");
+            </script>';
+            echo '<script>
+                 window.history.go(-1);
+            </script>';
+        }
+        
+        else if ($username != $results['username'] and $password != $results['password']){
+            echo '<script>
+                alert ("wrong username and password");
+            </script>';
+            echo '<script>
+             window.history.go(-1);
+        </script>';
+
+        }
+        else if ($username != $results['username']) {
+            echo '<script>
+                alert ("wrong Username");
+            </script>';
+            echo '<script>
+                 window.history.go(-1);
+            </script>';
+        }
+
+        else if ($password != $results['password']) {
+            echo '<script>
+                alert ("wrong password");
+            </script>';
+            echo '<script>
+                 window.history.go(-1);
+            </script>';
+        }
+
 	$count = $statement->rowcount();
 	if ($count > 0 ){
 		header('Location:../PROJECTCOBRA/index1.html');
